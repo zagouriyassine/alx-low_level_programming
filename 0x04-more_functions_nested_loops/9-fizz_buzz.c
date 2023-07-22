8f2b4f33d0fe5985d67c4b0df134675994a36b91
zagouriyassine <zagouriyassine8@gmail.com>
2023-07-21 19:03:09 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,12 +16,12 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && !(num % 5 == 0))
+		if ((num % 3 == 0) && (num % 5 == 0))
+			printf("FizBuzz");
+		else if (num % 3 == 0)
 			printf("Fizz");
-		else if (num % 5 == 0 && !(num % 3 == 0))
+		else if (num % 5 == 0)
 			printf("Buzz");
-		else if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
 		else
 			printf("%d", num);
 
