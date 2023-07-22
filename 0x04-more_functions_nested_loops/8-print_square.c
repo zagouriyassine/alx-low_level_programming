b10c867861a8beed05f2a70181a05a3a58240229
zagouriyassine <zagouriyassine8@gmail.com>
2023-07-21 18:52:37 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,10 +12,17 @@ void print_square(int size)
 {
 	int x, y;
 
-	for (x = 0; x <= size; x++)
+	if (size <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x <= size; x++)
+		{
 		for (y = 0; y <= size; y++)
 			_putchar(35);
+		}
 		_putchar('\n');
 	}
 }
