c044fcc177d52674761290b35dea9f0cca5b4296
zagouriyassine <zagouriyassine8@gmail.com>
2023-07-26 14:51:04 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "mian.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: input string
+ * Return: string with capitalized words
+*/
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
+	}
+	return (ptr);
+}
