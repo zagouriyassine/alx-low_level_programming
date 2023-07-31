a0c82ae6b0e9f1c6ec2f334f1d99ca490ac49c2d
zagouriyassine <zagouriyassine8@gmail.com>
2023-07-31 14:17:27 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,4 +3,25 @@
 /**
  * *_strpbrk - searches a string for any of a set of bytes.
  *
- * */
+ * @s: input
+ * @accept: input
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int Q;
+
+	while (*s)
+	{
+		for (Q = 0; accpet[Q]; Q++)
+		{
+			if (*s == accept[Q])
+			return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
